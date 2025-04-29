d0d5c4d2635ac13c39d60a1791e45556d893c3b7
ElioCandela <eliorubencandela@hotmail.com>
2025-04-29 07:56:29 -0500

    Cambios 2

--- src/aprendiendogit/AprendiendoGit.java
+++ src/aprendiendogit/AprendiendoGit.java
@@ -14,7 +14,8 @@ public class AprendiendoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("SOy el cambio1");
+        System.out.println("Soy el cambio 2");
     }
     
 }
